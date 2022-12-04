da34d929589c2023b4759faed960a2871e559925
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-12-04 16:01:34 +0100

    Day 4 input

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,7 +1,8 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
 
-        val out = input.flatMap { it.split(",") }
+        return input.flatMap { it.split(",") }
             .asSequence()
             .map { it.split("-") }
             .map { it[0].toInt()..it[1].toInt() }
@@ -9,25 +10,19 @@ fun main() {
             .map { (it[0] intersect it[1]).containsAll(it[0].toList()) || (it[0] intersect it[1]).containsAll(it[1].toList()) }
             .filter { it }
             .count()
-
-        return out
     }
 
     fun part2(input: List<String>): Int {
 
-        val out = input.flatMap { it.split(",") }
+        return input.flatMap { it.split(",") }
             .asSequence()
             .map { it.split("-") }
             .map { it[0].toInt()..it[1].toInt() }
             .chunked(2)
-            .map { it[0].toList().any { iter -> iter in it[1] } || it[1].toList().any { iter -> iter in it[0] } }
+            .map { it[0].any { iter -> iter in it[1] } || it[1].any { iter -> iter in it[0] } }
             .filter { it }
             .count()
-
-        return out
     }
-
-
 
     val input = readInput("../input4")
     println(part1(input))
