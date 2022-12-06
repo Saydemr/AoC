d7f2b95948f310b59893a5b3e0c05e4d798213f5
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-12-06 11:00:06 +0100

    Heyy, clean code in kotlin :sunglasses:

---
+++ src/Day06.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    fun uniqueSubstring(input: String, consecutive: Int): Int {
+
+        val firstUnique = input
+            .asSequence()
+            .windowed(consecutive,1,partialWindows = true)
+            .filter { it.distinct().size == it.size }
+            .first()
+            .joinToString { it.toString() }
+            .replace(", ", "")
+
+
+        return input.indexOf(firstUnique) + consecutive
+
+    }
+
+
+    val input = readInput("../input6")
+    println(uniqueSubstring(input[0],4))
+    println(uniqueSubstring(input[0],14))
+}
