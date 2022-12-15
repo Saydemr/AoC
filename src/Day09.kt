7b114502e4ad842a763ff32f503da5bf19300835
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-12-15 02:17:09 +0100

    Refurbish

--- src/Day09.kt
+++ src/Day09.kt
@@ -21,8 +21,38 @@ fun main() {
     val visitedCells = mutableSetOf<Pair<Int,Int>>()
 
 
+    fun part1(input: List<List<String>>): Int {
+
+        visitedCells.clear()
+        val knotPositions = mutableListOf(
+            Pair(0, 0), Pair(0, 0))
+
+        input.forEach {
+            val direction = lookup[it[0]]
+            val distance = it[1].toInt()
+
+            for (a in 0 until distance) {
+                knotPositions[0] = Pair(first = knotPositions[0].first + direction!!.first,
+                    second = knotPositions[0].second + direction.second )
+
+                for (index in 1 until knotPositions.size) {
+                    val diff = Pair(
+                        first = knotPositions[index].first - knotPositions[index - 1].first,
+                        second = knotPositions[index].second - knotPositions[index - 1].second
+                    )
+
+                    knotPositions[index] = Pair(
+                        first = knotPositions[index - 1].first + correct(diff).first,
+                        second = knotPositions[index - 1].second + correct(diff).second
+                    )
+                }
+                visitedCells.add(knotPositions.last())
+            }
+        }
+        return visitedCells.size
+    }
+
     fun part2(input: List<List<String>>): Int {
-        // create a mutable list of 9 Pair(0,0)
 
         visitedCells.clear()
         val knotPositions = mutableListOf(
@@ -51,22 +81,17 @@ fun main() {
                         second = knotPositions[index - 1].second + correct(diff).second
                     )
                 }
-                println(knotPositions)
                 visitedCells.add(knotPositions.last())
             }
-
-
         }
-
         return visitedCells.size
-
     }
 
     val input = readInput("../inputs/input9")
         .map { it.trim().split(" ") }
         .toList()
 
-    println("aaaaaa")
+    println(part1(input))
     println(part2(input))
 
 
